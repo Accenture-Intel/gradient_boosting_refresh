1f2033e9f0f10e679dde8aea2860281d12d88268
bgaguro <88739896+bgaguro@users.noreply.github.com>
2022-04-04 16:00:35 -0700

    Update xgboost_hist_method_bench.py

--- xgboost_hist_method_bench.py
+++ xgboost_hist_method_bench.py
@@ -1,12 +1,14 @@
 import argparse
 import xgboost as xgb
 import daal4py as d4p
+import time
 from bench_utils import *
 
 N_PERF_RUNS = 5
 DTYPE=np.float32
 
 xgb_params = {
+    'verbosity':                    0,
     'alpha':                        0.9,
     'max_bin':                      256,
     'scale_pos_weight':             2,
@@ -15,32 +17,34 @@ xgb_params = {
     'reg_lambda':                   1,
     "min_child_weight":             0,
     'max_depth':                    8,
-    'min-child-weight':             1,
     'max_leaves':                   2**8,
+    'objective':                    'binary:logistic',
+    'predictor':                    'cpu_predictor',
     'tree_method':                  'hist',
     'n_estimators':                 1000
 }
 
 def xbg_fit():
     global model_xgb, daal_model
-#     dtrain = xgb.DMatrix(x_train, label=y_train)
-#     model_xgb = xgb.train(xgb_params, dtrain, xgb_params['n_estimators'])
-#     daal_model = d4p.get_gbt_model_from_xgboost(model_xgb)
-    model_xgb = xgb.XGBClassifier(xgb_params)
-    model_xgb.fit(x_train, y_train)
+    dtrain = xgb.DMatrix(x_train, label=y_train)
+    model_xgb = xgb.train(xgb_params, dtrain, xgb_params['n_estimators'])
+    daal_model = d4p.get_gbt_model_from_xgboost(model_xgb)
+
 
 
 def xgb_predict_of_train_data():
-    global result_predict_xgb_train, daal_prediction_train
+    global daal_prediction_train, pred_train_time
     dtest = xgb.DMatrix(x_train)
-    # result_predict_xgb_train = model_xgb.predict(dtest)
+    start = time.time()
     daal_prediction = d4p.gbt_classification_prediction(nClasses = n_classes).compute(dtest, daal_model)
+    pred_train_time = time.time() - start
 
 def xgb_predict_of_test_data():
-    global result_predict_xgb_test, daal_prediction_test
+    global daal_prediction_test, pred_test_time
     dtest = xgb.DMatrix(x_test)
-    # result_predict_xgb_test = model_xgb.predict(dtest)
+    start = time.time()
     daal_prediction = d4p.gbt_classification_prediction(nClasses = n_classes).compute(dtest, daal_model)
+    pred_test_time = time.time() - start
 
 
 def load_dataset(dataset):
@@ -59,40 +63,41 @@ def load_dataset(dataset):
 
     x_train, y_train, x_test, y_test, n_classes = datasets_dict[dataset](DTYPE)
     print("n_classes: ", n_classes)
-    if n_classes == -1:
-        xgb_params['objective'] = 'reg:squarederror'
-    elif n_classes == 2:
-        xgb_params['objective'] = 'binary:logistic'
-    else:
-        xgb_params['objective'] = 'multi:softprob'
-        xgb_params['num_class'] = n_classes
+
+#     if n_classes == -1:
+#         xgb_params['objective'] = 'reg:squarederror'
+#     elif n_classes == 2:
+#         xgb_params['objective'] = 'binary:logistic'
+#     else:
+#         xgb_params['objective'] = 'multi:softprob'
+#         xgb_params['num_class'] = n_classes
 
 def parse_args():
     global N_PERF_RUNS
     parser = argparse.ArgumentParser()
     parser.add_argument('--n_iter', required=False, type=int, default=1000)
     parser.add_argument('--n_runs', default=N_PERF_RUNS, required=False, type=int)
-    parser.add_argument('--hw', choices=['cpu', 'gpu'], metavar='stage', required=False, default='cpu')
-    parser.add_argument('--log', metavar='stage', required=False, type=bool, default=False)
+#     parser.add_argument('--hw', choices=['cpu', 'gpu'], metavar='stage', required=False, default='cpu')
+#     parser.add_argument('--log', metavar='stage', required=False, type=bool, default=False)
     parser.add_argument('--dataset', choices=['higgs1m', "airline-ohe", "msrank-10k"],
             metavar='stage', required=True)
 
     args = parser.parse_args()
     N_PERF_RUNS = args.n_runs
 
-    xgb_params['n_estimators'] = args.n_iter
+#     xgb_params['n_estimators'] = args.n_iter
 
-    if args.log:
-        xgb_params['verbosity'] = 3
-    else:
-         xgb_params['silent'] = 1
+#     if args.log:
+#         xgb_params['verbosity'] = 3
+#     else:
+#          xgb_params['silent'] = 1
 
-    if args.hw == "cpu":
-        xgb_params['tree_method'] = 'hist'
-        xgb_params['predictor']   = 'cpu_predictor'
-    elif args.hw == "gpu":
-        xgb_params['tree_method'] = 'gpu_hist'
-        xgb_params['predictor']   = 'gpu_predictor'
+#     if args.hw == "cpu":
+#         xgb_params['tree_method'] = 'hist'
+#         xgb_params['predictor']   = 'cpu_predictor'
+#     elif args.hw == "gpu":
+#         xgb_params['tree_method'] = 'gpu_hist'
+#         xgb_params['predictor']   = 'gpu_predictor'
 
     load_dataset(args.dataset)
 
@@ -104,7 +109,10 @@ def main():
     measure(xbg_fit,                   "XGBOOST training            ", N_PERF_RUNS)
     measure(xgb_predict_of_train_data, "XGBOOST predict (train data)", N_PERF_RUNS)
     measure(xgb_predict_of_test_data,  "XGBOOST predict (test data) ", N_PERF_RUNS)
-
+    
+    print("Prediction Train Dataset Time: ", pred_train_time)
+    print("Prediction Test Dataset Time: ", pred_test_time)
+    
     print("Compute quality metrics...")
 
 #     train_loglos = compute_logloss(y_train, result_predict_xgb_train)
