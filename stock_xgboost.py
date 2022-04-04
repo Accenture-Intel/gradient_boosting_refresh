af73bba98deeca53a1601a3b366d5aa24c90c37b
bgaguro <88739896+bgaguro@users.noreply.github.com>
2022-04-04 15:10:25 -0700

    Update stock_xgboost.py

--- stock_xgboost.py
+++ stock_xgboost.py
@@ -1,12 +1,13 @@
 import argparse
 import xgboost as xgb
 from bench_utils import *
+import time
 
 N_PERF_RUNS = 5
 DTYPE=np.float32
 
 xgb_params = {
-#     'verbosity':                    0,
+    'verbosity':                    0,
     'alpha':                        0.9,
     'max_bin':                      256,
     'scale_pos_weight':             2,
@@ -15,25 +16,31 @@ xgb_params = {
     'reg_lambda':                   1,
     "min_child_weight":             0,
     'max_depth':                    8,
-    'max_leaves':                   2**8
-#     'predictor':                    'cpu_predictor',
-#     'tree_method':                  'hist',
-#     'n_estimators':                 1000
+    'max_leaves':                   2**8,
+    'objective':                    'binary:logistic',
+    'predictor':                    'cpu_predictor',
+    'tree_method':                  'hist',
+    'n_estimators':                 1000
 }
 
 def xbg_fit():
-    global model_xgb
+    global model_xgb, train_time
     model_xgb = xgb.XGBClassifier(xgb_params)
+    start = time.time()
     model_xgb.fit(x_train, y_train)
-
+    train_time = time.time() - start
+    
 def xgb_predict_of_train_data():
-    global result_predict_xgb_train
+    global result_predict_xgb_train, pred_train_time
+    start = time.time()
     result_predict_xgb_train = model_xgb.predict(x_train)
-
+    pred_train_time = time.time() - start
+    
 def xgb_predict_of_test_data():
     global result_predict_xgb_test
+    start = time.time()
     result_predict_xgb_test = model_xgb.predict(xtest)
-
+    pred_test_time = time.time() - start
 
 def load_dataset(dataset):
     global x_train, y_train, x_test, y_test
@@ -51,40 +58,17 @@ def load_dataset(dataset):
 
     x_train, y_train, x_test, y_test, n_classes = datasets_dict[dataset](DTYPE)
 
-    if n_classes == -1:
-        xgb_params['objective'] = 'reg:squarederror'
-    elif n_classes == 2:
-        xgb_params['objective'] = 'binary:logistic'
-    else:
-        xgb_params['objective'] = 'multi:softprob'
-        xgb_params['num_class'] = n_classes
-
 def parse_args():
     global N_PERF_RUNS
     parser = argparse.ArgumentParser()
-    parser.add_argument('--n_iter', required=False, type=int, default=1000)
-    parser.add_argument('--n_runs', default=N_PERF_RUNS, required=False, type=int)
-    parser.add_argument('--hw', choices=['cpu', 'gpu'], metavar='stage', required=False, default='cpu')
-    parser.add_argument('--log', metavar='stage', required=False, type=bool, default=False)
+#     parser.add_argument('--n_iter', required=False, type=int, default=1000)
+#     parser.add_argument('--n_runs', default=N_PERF_RUNS, required=False, type=int)
+#     parser.add_argument('--hw', choices=['cpu', 'gpu'], metavar='stage', required=False, default='cpu')
     parser.add_argument('--dataset', choices=['higgs1m', "airline-ohe", "msrank-10k"],
             metavar='stage', required=True)
 
     args = parser.parse_args()
-    N_PERF_RUNS = args.n_runs
 
-    xgb_params['n_estimators'] = args.n_iter
-
-    if args.log:
-        xgb_params['verbosity'] = 3
-    else:
-         xgb_params['silent'] = 1
-
-    if args.hw == "cpu":
-        xgb_params['tree_method'] = 'hist'
-        xgb_params['predictor']   = 'cpu_predictor'
-    elif args.hw == "gpu":
-        xgb_params['tree_method'] = 'gpu_hist'
-        xgb_params['predictor']   = 'gpu_predictor'
 
     load_dataset(args.dataset)
 
@@ -97,6 +81,10 @@ def main():
     measure(xgb_predict_of_train_data, "XGBOOST predict (train data)", N_PERF_RUNS)
     measure(xgb_predict_of_test_data,  "XGBOOST predict (test data) ", N_PERF_RUNS)
 
+    print("Training Time: ", train_time)
+    print("Predicting Training Data Time: ", pred_train_time)
+    print("Predicting Testing Data Time: ", pred_test_time)
+    
     print("Compute quality metrics...")
 
     train_loglos = compute_logloss(y_train, result_predict_xgb_train)
